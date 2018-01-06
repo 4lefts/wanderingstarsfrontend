fef2978a0b432b953fae3f8b60cfebe052175228
Stephen Ball <5t3ph3n.ball@gmail.com>
2018-01-06 15:22:50 +0000

    added text to visualisation

--- src/visualiser.js
+++ src/visualiser.js
@@ -8,7 +8,8 @@ let container = document.getElementById('sky-container')
 let sz
 
 function renderSky(data){
-    const cartesianData = makeCartesian(data)
+    const filteredData = data.filter(body => body.hasOwnProperty('set'))
+    const cartesianData = makeCartesian(filteredData)
     const sz = container.offsetWidth
     console.log(cartesianData)
     patch(node, (node = skyView(cartesianData, sz)), container)
@@ -17,24 +18,31 @@ function renderSky(data){
 function skyView(state, size){
     return h('svg', {
         "xmlns":  "http://www.w3.org/2000/svg", 
-        "height":  sz, 
-        "width":  sz,
-        "viewBox": "-100 -100 200 200",
+        "height":  size, 
+        "width":  size,
+        "viewBox": `${-(size / 2)} ${-(size / 2)} ${size} ${size}`,
     }, [h('circle', {
             "cx": "0",
             "cy": "0",
-            "r": "100",
+            "r": `${size / 2}`,
             "stroke": "none",
             "fill": "#010125",
         }),
         state.map(b => {
-            return (h('circle', {
-                "cx": b.x,
-                "cy": b.y,
-                "r": "10",
+            const _x = b.x * size / 2
+            const _y = b.y * size / 2
+            return ([h('circle', {
+                "cx": _x,
+                "cy": _y,
+                "r": "2",
                 "stroke": "none",
-                "fill": "#ff0000",
-            }))
+                "fill": "firebrick",
+            }), h('text', {
+                "x": _x + 5,
+                "y": _y + 2,
+                "stroke": "none",
+                "fill": "firebrick",
+            }, b.name)])
         })
     ])
 }
@@ -42,9 +50,9 @@ function skyView(state, size){
 //helper functions
 function makeCartesian(arr){
     return arr.map(body => {
-        //get radius from zenith, not altitude above horizon
-        const radius = 180 - angleToDecimal(body.alt)
-        const theta = degreesToRadians(angleToDecimal(body.az))
+        //get radius from zenith, not altitude above horizon, in range 0 - 1
+        const radius = (180 - angleToDecimal(body.alt)) / 180
+        const theta = degreesToRadians(angleToDecimal(body.az)) - (Math.PI / 2)
         return {
             name: body.name,
             x: radius * Math.cos(theta),
