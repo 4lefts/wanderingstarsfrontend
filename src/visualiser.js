a290043d0f06365df04280336d768709341a48d1
Stephen Ball <5t3ph3n.ball@gmail.com>
2018-01-06 16:33:12 +0000

    added labels

--- src/visualiser.js
+++ src/visualiser.js
@@ -28,21 +28,38 @@ function skyView(state, size){
             "stroke": "none",
             "fill": "#010125",
         }),
+        state.map((b, idx) => {
+            const x1 = b.x * size / 4
+            const x2 = b.x * size / 2
+            const y1 = idx * -20 
+            const y2 = b.y * size / 2 
+            return (h('line', {
+                x1,
+                y1,
+                x2,
+                y2,
+                "stroke": "#331122",
+                "stroke-width": "2"
+            }))            
+        }),
+        state.map((b, idx) => {
+            return (h('text', {
+                "x": b.x * size / 4,
+                "y": idx * - 20,
+                "stroke": "none",
+                "fill": "firebrick",
+            }, b.name))
+        }),
         state.map(b => {
             const _x = b.x * size / 2
             const _y = b.y * size / 2
-            return ([h('circle', {
+            return (h('circle', {
                 "cx": _x,
                 "cy": _y,
-                "r": "2",
+                "r": "5",
                 "stroke": "none",
                 "fill": "firebrick",
-            }), h('text', {
-                "x": _x + 5,
-                "y": _y + 2,
-                "stroke": "none",
-                "fill": "firebrick",
-            }, b.name)])
+            }))            
         })
     ])
 }
